037ea0fff1db06486365f228f1af054aee869c52
Tamas Michelberger <tomi.michel@gmail.com>
2014-03-04 23:54:46 +0100

    added page files, fixed links in layout

--- app/app.rb
+++ app/app.rb
@@ -7,3 +7,23 @@ set :public_folder, File.dirname(__FILE__) + '/assets'
 get "/" do
   erb :index
 end
+
+get "/eloadok" do
+  erb :speakers
+end
+
+get "/info" do
+  erb :info
+end
+
+get "/kapcsolat" do
+  erb :contact
+end
+
+get "/partnerek" do
+  erb :partners
+end
+
+get "/regisztracio" do
+  erb :signup
+end
