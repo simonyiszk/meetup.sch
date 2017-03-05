be6c72b13c3193e4b5adeb5fe5f11a81f4afece7
Bokros Bálint <bbalint96@gmail.com>
2017-03-05 22:06:43 +0100

    Update registration time

--- meetup/routes/email.js
+++ meetup/routes/email.js
@@ -11,7 +11,7 @@ db.run('CREATE TABLE IF NOT EXISTS ' + sqlname + ' (id INTEGER PRIMARY KEY AUTOI
 /* post registration. */
 router.post('/', function(req, res) {
 
-    if (date.getTime() > new Date(2016,10,18,00,00,00).getTime()) {
+    if (date.getTime() > new Date(2017,03,15,00,00,00).getTime()) {
 
         res.send('A regisztráció időszaka lezárult.')
     }
